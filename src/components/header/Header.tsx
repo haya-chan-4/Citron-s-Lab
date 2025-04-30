0d3a61bba0bf502bb1ca776e33bd681d86d62d22
Hayata <hayata8812@gmail.com>
2025-04-30 17:57:43 +0900

    Suspense導入

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,29 +1,33 @@
 import Link from "next/link";
-
+import { Suspense } from "react";
+import Spinner from './Spinner';
 
 interface Props {
-  title: string
+  title: string;
 }
+
 
 const Header = (props: Props): JSX.Element => {
   const { title } = props;
 
   return (
-    <header className="bg-gray-800 shadow-md w-screen m-0">
-      <div className="max-w-5xl mx-auto px-4 py-4 flex items-center justify-between">
-        <Link href='/' className="text-xl font-bold text-gray-400">
-          {title}
-        </Link>
-        <nav className="space-x-4">
-          <Link href="/" className="text-gray-400 hover:text-blue-600 transition-colors">
-            ホーム
+    <Suspense fallback={<Spinner />}>
+      <header className="bg-teal-800 shadow-md w-screen m-0">
+        <div className="max-w-5xl mx-auto px-4 py-4 flex items-center justify-between">
+          <Link href='/' className="text-xl font-bold text-gray-400">
+            {title}
           </Link>
-          <Link href="/about" className="text-gray-400 hover:text-blue-600 transition-colors">
-            このサイトについて
-          </Link>
-        </nav>
-      </div>
-    </header>
+          <nav className="space-x-4">
+            <Link href="/" className="text-gray-400 hover:text-blue-600 transition-colors">
+              ホーム
+            </Link>
+            <Link href="/about" className="text-gray-400 hover:text-blue-600 transition-colors">
+              このサイトについて
+            </Link>
+          </nav>
+        </div>
+      </header>
+    </Suspense>
   )
 }
 
