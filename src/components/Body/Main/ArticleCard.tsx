402d98e866a10ddf11928d7e6fdf24932acb111c
Hayata <hayata8812@gmail.com>
2025-05-02 19:57:31 +0900

    ArticleCardコンポーネントのレイアウトを修正し、タイトル、カテゴリ、投稿日をグリッド形式で表示するように変更。

--- src/components/Body/Main/ArticleCard.tsx
+++ src/components/Body/Main/ArticleCard.tsx
@@ -31,14 +31,18 @@ const ArticleCard = (props: Props): JSX.Element => {
             alt="サムネ画像"
           />
         </div>
-        <div className="flex-1 ">
-          <h2 className=" text-lg font-semibold sm:mb-2 text-gray-700 p-5 mb-8">{blog.title}</h2>
-          <p className="text-sm text-indigo-600 border border-indigo-600 rounded px-2 pt-[3px] pb-[2px]  absolute sm:left-72  left-6 bottom-3">
-            {blog.category}
-          </p>
-          <p className="text-sm absolute sm:right-5 sm:pr-5 right-2 bottom-3 pr-3 text-gray-500">
-            投稿日: {new Date(blog.publishedAt ?? '').toLocaleDateString()}
-          </p>
+        <div className="grid grid-cols-1 w-full">
+          <h2 className="grid-span-2 text-lg font-semibold text-gray-700 p-5 mg-8">
+            {blog.title}
+          </h2>
+          <div className='grid text-lg text-gray-700 px-5 pb-4'>
+            <p className="w-max h-min grid-item col-span-1 text-sm text-indigo-600 border border-indigo-600 rounded px-2 pt-[3px] pb-[2px] mb-2">
+              {blog.category}
+            </p>
+            <p className="text-sm text-gray-500">
+              投稿日: {new Date(blog.publishedAt ?? '').toLocaleDateString()}
+            </p>
+          </div>
         </div>
       </div>
     </Link>
