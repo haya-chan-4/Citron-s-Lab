6defb2ea7af6a667d53b5528cbd6de9affe141de
Hayata <hayata8812@gmail.com>
2025-05-02 13:21:02 +0900

    サイドバーにカテゴリ表示機能を追加し、ブログのカテゴリ情報を取得するように変更

--- src/components/Body/SideBar/SideBar.tsx
+++ src/components/Body/SideBar/SideBar.tsx
@@ -1,7 +1,7 @@
 import { client } from "@/libs/client";
 import { Blog } from "@/types/blog";
-import Link from "next/link";
 import LatestCard from "./LatestCard";
+import CategoryItem from "./CategoryItem";
 
 
 type Props = object
@@ -10,64 +10,50 @@ const getBlogs = async (): Promise<Blog[]> => {
     endpoint: 'blog',
     queries: {
       limit: 3,
-      fields: 'id,title,publishedAt,thumbnail'
+      fields: 'id,title,publishedAt,thumbnail,category'
     },
   });
   return data.contents;
 };
 
-const SideBar = async (props: Props): Promise<JSX.Element> => {
+const SideBar: React.FC<Props> = async (props) => {
   const blogs = await getBlogs();
-  const {} = props;
+  const { } = props;
   return (
     <aside className="hidden lg:block w-80 space-y-6">
-        {/* カテゴリー一覧 */}
-        <section className="bg-white p-4 ">
-          <h2 className="text-lg bg-gray-200 p-2 rounded-md font-semibold mb-3">カテゴリ</h2>
-          <ul className="space-y-0 divide-y divide-gray-200 text-gray-700">
-            <li className="py-2">
-              <Link href="/blog?category=tech" className="hover:text-gray-900 pl-2">
-                Tech
-              </Link>
-            </li>
-            <li className="py-2">
-              <Link href="/blog?category=design" className="hover:text-gray-900 pl-2">
-                Design
-              </Link>
-            </li>
-            <li className="py-2">
-              <Link href="/blog?category=life" className="hover:text-gray-900 pl-2">
-                Life
-              </Link>
-            </li>
-            <li className="py-2">
-              <Link href="/blog?category=career" className="hover:text-gray-900 pl-2">
-                Career
-              </Link>
-            </li>
-          </ul>
-        </section>
+      {/* カテゴリー一覧 */}
+      <section className="bg-white p-4 ">
+        <h2 className="text-lg bg-gray-200 p-2 rounded-md font-semibold mb-3">カテゴリ</h2>
+        <ul className="space-y-0 divide-y divide-gray-200 text-gray-700">
+          {blogs.map((blog) => (
+            <CategoryItem
+              key={blog.id}
+              category={blog.category}
+            />
+          ))}
+        </ul>
+      </section>
 
-        {/* 最新記事 */}
-        <section className="bg-white p-4">
-          <h2 className="text-lg bg-gray-200 p-2 rounded-md font-semibold mb-3">最新記事</h2>
-          <ul className="space-y-2">
-            {/* 例: thisMonthPosts.map */}
-            {blogs.map((blog) => (
-              <LatestCard
-                key={blog.id}
-                blog={{
-                  id: blog.id,
-                  thumbnail: {
-                    url: blog.thumbnail.url
-                  },
-                  title: blog.title,
-                  publishedAt: blog.publishedAt ?? ''
-                }} />
-            ))}
-          </ul>
-        </section>
-      </aside>
+      {/* 最新記事 */}
+      <section className="bg-white p-4">
+        <h2 className="text-lg bg-gray-200 p-2 rounded-md font-semibold mb-3">最新記事</h2>
+        <ul className="space-y-2">
+          {/* 例: thisMonthPosts.map */}
+          {blogs.map((blog) => (
+            <LatestCard
+              key={blog.id}
+              blog={{
+                id: blog.id,
+                title: blog.title,
+                publishedAt: blog.publishedAt ?? '',
+                thumbnail: {
+                  url: blog.thumbnail.url
+                },
+              }} />
+          ))}
+        </ul>
+      </section>
+    </aside>
   )
 }
 
