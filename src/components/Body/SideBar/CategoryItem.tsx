82bb64cbaee9cc1f886e4ab1983bf0c7a6450167
Hayata <hayata8812@gmail.com>
2025-05-02 20:31:51 +0900

    カテゴリアイテムコンポーネントのリンク先を「/blog?category=tech」から「/」に変更

--- src/components/Body/SideBar/CategoryItem.tsx
+++ src/components/Body/SideBar/CategoryItem.tsx
@@ -5,11 +5,13 @@ interface Props {
 category: string
 }
 
+
+
 const CategoryItem: React.FC<Props> = (props) => {
   const { category } = props;
   return (
     <li className="py-2">
-      <Link href="/blog?category=tech" className="hover:text-gray-900 pl-2">
+      <Link href="/" className="hover:text-gray-900 pl-2">
         {category}
       </Link>
     </li>
