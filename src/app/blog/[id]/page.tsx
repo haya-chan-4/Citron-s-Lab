83fb5ee58ba4b43f5bdcacaf10cb5c382da63278
Hayata <hayata8812@gmail.com>
2025-05-04 14:01:15 +0900

    ブログ記事ページのスタイルを改善し、不要なCommentコンポーネントのインポートをコメントアウト。メインコンテンツのレイアウトを調整し、画像表示の設定を修正。

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -3,7 +3,7 @@ import { client } from '@/libs/client'
 import { BlogPost } from "@/types/blogPost"
 import Image from 'next/image'
 // import Link from 'next/link'
-import Comment from '@/components/Comment/Comment'
+// import Comment from '@/components/Comment/Comment'
 import SideBar from '@/components/Body/SideBar/SideBar'
 import MetaInfo from '@/components/Body/Main/MetaInfo'
 import LocationOfPage from '@/components/Body/Main/LocationOfPage'
@@ -23,25 +23,25 @@ const BlogPostPage = async ({ params }: { params: { id: string } }) => {
   const post = await getBlogPost(params.id)
 
   return (
-    <main className="flex flex-col lg:flex-row max-w-screen-xl mx-auto px-4 py-10 gap-8 animate-fadeIn">
+    <main className="flex flex-col lg:flex-row max-w-screen-xl gap-8 py-10 sm:px-4 md:px-24 lg:pl-24">
       {/* メインコンテンツ */}
-      <article className="flex-1">
+      <article className="flex-1 animate-fadeIn mx-4">
         {/* パンくず */}
         <LocationOfPage category={post.category} />
         {/* アイキャッチ画像 */}
         {post.thumbnail?.url && (
-          <div className="relative w-full aspect-video mb-6 rounded-md overflow-hidden shadow-sm">
+          <div className="relative aspect-video mb-6 rounded-md overflow-hidden shadow-sm">
             <Image
               src={post.thumbnail.url}
               alt={post.title}
-              fill
               className="object-cover"
+              fill
             />
           </div>
         )}
 
         {/* タイトル */}
-        <h1 className="text-3xl font-bold mb-4 text-gray-800">
+        <h1 className="text-3xl font-bold mb-4 text-gray-800  sm:w-full">
           {post.title}
         </h1>
 
@@ -49,10 +49,10 @@ const BlogPostPage = async ({ params }: { params: { id: string } }) => {
         <MetaInfo publishedAt={post.publishedAt} category={post.category} />
 
         {/* 本文 */}
-        <div className="prose prose-sm md:prose-lg prose-headings:font-semibold prose-img:rounded-lg prose-a:text-blue-600">
+        <div className="prose max-w-full prose-sm md:prose-lg prose-headings:font-semibold prose-img:rounded-sm prose-a:text-blue-600 sm:w-full ">
           <div dangerouslySetInnerHTML={{ __html: post.body }} />
         </div>
-        <Comment blogId={''} />
+        {/* <Comment blogId={''} /> */}
       </article>
       {/* サイドバー */}
       <SideBar />
