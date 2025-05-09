2722ea4b537c3369cbd1f8ea2eca6839e7f734cd
Hayata <hayata8812@gmail.com>
2025-05-09 11:41:53 +0900

    NotFoundコンポーネントのスタイルを調整し、レイアウトを改善。flexboxを使用して、レスポンシブデザインを強化。

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,9 @@
 // app/not-found.tsx
 export default function NotFound() {
   return (
-    <main className="flex-1">
-      <h1>404</h1>
-      <p>ページが見つかりませんでした</p>
+    <main className="flex-1 flex-col lg:flex-row py-10 sm:px-4 md:px-24 ">
+      <h1 className="">404</h1>
+      <p className="">ページが見つかりませんでした</p>
     </main>
   );
 }
