c21880bac7fd43f5ea420f849d53cc1b2cb900ba
Hayata <hayata8812@gmail.com>
2025-05-14 00:33:31 +0900

    LocationOfPageコンポーネントでカテゴリ表示をCategoryDisplayからリンクに変更し、formatCategoryName関数を使用してカテゴリ名の表示形式を統一しました。また、不要なコードを削除しました。

--- src/components/Body/Main/LocationOfPage.tsx
+++ src/components/Body/Main/LocationOfPage.tsx
@@ -1,26 +1,23 @@
 // src/components/Body/Main/LocationOfPage.tsx
-import Link from "next/link"
-import CategoryDisplay from "@/components/UI/CategoryDisplay"
-
-
+import Link from "next/link";
+import { formatCategoryName } from '@/utils/format'
 interface LocationOfPageProps {
   category: string
+
   page: string
 }
-
 const LocationOfPage: React.FC<LocationOfPageProps> = (props): JSX.Element => {
   const { category, page } = props
-
-
   return (
     <nav className="text-gray-500 text-md font-bold mb-8">
       <Link href="/" className="hover:underline">
         {page}
       </Link>
       <span className="mx-2">{'>'}</span>
-      <CategoryDisplay categoryId={category} styleType="link" />
+      <Link href={`/category/${category}`} className="text-indigo-500 hover:underline">
+        {formatCategoryName(category)}
+      </Link>
     </nav>
   )
 }
-
 export default LocationOfPage
